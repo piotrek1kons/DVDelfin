112b0e8a03b2b2e764166b5bfaa03547eefde7a3
piotrek1kons <119406083+piotrek1kons@users.noreply.github.com>
2023-12-29 16:55:15 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
+    public static void main(String[] args) {
+        Rejestracja rejestracja = new Rejestracja();
+        rejestracja.setBackground(new Color(248, 249, 241));
+        rejestracja.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rejestracja.setVisible(true);
+
+    }
+
+}
