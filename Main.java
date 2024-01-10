e60941495b433e8d331b495e7f28fa25ebe05f8a
piotrek1kons <119406083+piotrek1kons@users.noreply.github.com>
2024-01-10 15:06:04 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Main extends JFrame{
+    static OknoWejsciowe o;
 
     public static void main(String[] args) {
-        Rejestracja rejestracja = new Rejestracja();
-        rejestracja.setBackground(new Color(248, 249, 241));
-        rejestracja.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        rejestracja.setVisible(true);
+        o = new OknoWejsciowe();
+        o.setContentPane(o.getPanelStartowy());
+        o.setTitle("DVDelfin - Start");
+        o.setSize(300,400);
+        o.setVisible(true);
+        o.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        o.setLocationRelativeTo(null);
+    }
 
+    public static void closeMainWindow() {
+        o.dispose();
     }
 
 }
