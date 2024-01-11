557f89b052adbc0a40a7272f0edf2e2b98a4cdb1
odlugiewicz <odlugiewicz.9@gmail.com>
2024-01-11 16:15:07 +0100

    dodałam okno menu głównego, ale trzeba podpiać to i stworzyć nowe okna

---
+++ MenuGlowne.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuGlowne {
+    private JPanel panelGlowny;
+    private JPanel panelTytulowy;
+    private JButton buttonWypozyczFilm;
+    private JButton buttonOddajFilm;
+    private JButton buttonSprawdzWypozyczoneFilmy;
+    private JButton buttonUstawienia;
+    private JButton buttonWyloguj;
+
+    public JPanel getPanelMenuGlownego() {
+        return panelGlowny;
+    }
+    public JPanel getPanelTytulowy() {
+        return panelTytulowy;
+    }
+
+    public MenuGlowne(){
+        buttonWypozyczFilm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(buttonWypozyczFilm,"Wybrałeś wypozyczenie filmu");
+
+            }
+        });
+        buttonOddajFilm.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(buttonOddajFilm,"Wybrałeś oddanie filmu");
+
+                }
+            });
+        buttonSprawdzWypozyczoneFilmy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(buttonSprawdzWypozyczoneFilmy,"Wybrałeś sprawdzenie wypozyczonych filmów");
+
+            }
+        });
+        buttonUstawienia.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(buttonUstawienia,"Wybrałeś ustawienia");
+
+            }
+        });
+        buttonWyloguj.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(buttonWyloguj,"Wybrałeś wylogowanie");
+
+            }
+        });
+
+
+    }
+}
