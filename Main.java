ebc637823434075ee8a00659fa0ed44e30727bee
odlugiewicz <odlugiewicz.9@gmail.com>
2024-01-11 11:43:12 +0100

    TEST ola: dodałam enter w main xd

--- Main.java
+++ Main.java
@@ -14,6 +14,7 @@ public class Main extends JFrame{
         o.setVisible(true);
         o.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         o.setLocationRelativeTo(null);
+
     }
 
     public static void closeMainWindow() {
